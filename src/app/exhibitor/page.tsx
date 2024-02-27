8f0892ca9c629792e6fc72865ccdb2563e243c15
August Paulsrud <augustpaulsrud@gmail.com>
2024-02-27 19:52:55 +0100

    Added buttons, placeholder txt to "for companies"

--- src/app/exhibitor/page.tsx
+++ src/app/exhibitor/page.tsx
@@ -6,12 +6,33 @@ export default function ForExhibitorsPage() {
 		<Page.Background withIndents>
 			<Page.Boundary maxWidth={600}>
 				<Page.Header tier="primary">For Exhibitors</Page.Header>
-				<div className="h-5" />
-				<a href="https://register.armada.nu/register">
-					<Button>Signup to be part of armada</Button>
-				</a>
-				<p className="leading-8 text-stone-400">text here</p>
-				<p>timeline here</p>
+				<div className="h-4" />
+
+				<div className="flex flex-col space-y-4">
+					<div className="mt-2 flex flex-row flex-wrap justify-stretch gap-4 ">
+						<a href="https://register.armada.nu/register">
+							<Button>Signup to armada</Button>
+						</a>
+						<a href="/exhibitor/packages">
+							<Button variant={"secondary"} className="dark:bg-liqorice-700">
+								Packages
+							</Button>
+						</a>
+						<a href="/exhibitor/timeline">
+							<Button variant={"secondary"} className="dark:bg-liqorice-700">
+								Timeline
+							</Button>
+						</a>
+						<a href="/exhibitor/why_kth">
+							<Button variant={"secondary"} className="dark:bg-liqorice-700">
+								Why KTH
+							</Button>
+						</a>
+					</div>
+				</div>
+				<div className="mt-4">
+					<p>text here</p>
+				</div>
 			</Page.Boundary>
 		</Page.Background>
 	)
