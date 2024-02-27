c5b179d3525742ddb0ffd40b50d7458dc2d6359f
August Paulsrud <augustpaulsrud@gmail.com>
2024-02-27 16:49:30 +0100

    added signup button to exhibitors page

--- src/app/exhibitor/page.tsx
+++ src/app/exhibitor/page.tsx
@@ -1,12 +1,17 @@
 import { Page } from "@/components/shared/Page"
+import { Button } from "@/components/ui/button"
 
 export default function ForExhibitorsPage() {
 	return (
 		<Page.Background withIndents>
 			<Page.Boundary maxWidth={600}>
-				<Page.Header tier="primary">text here</Page.Header>
+				<Page.Header tier="primary">For Exhibitors</Page.Header>
 				<div className="h-5" />
+				<a href="https://register.armada.nu/register">
+					<Button>Signup to be part of armada</Button>
+				</a>
 				<p className="leading-8 text-stone-400">text here</p>
+				<p>timeline here</p>
 			</Page.Boundary>
 		</Page.Background>
 	)
