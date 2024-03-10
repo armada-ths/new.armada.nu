c6a67278de5bb9e2c1e2cbc68c71460409dabe06
Hampus Hallkvist <hampus@hallkvist.org>
2024-03-10 12:33:25 +0100

    fix: Redirect recruitment

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "armada-ais-files.s3.amazonaws.com",
-            port: "",
-            pathname: "**"
-        },
-        {
-            protocol: "https",
-            hostname: "s3.amazonaws.com",
-            port: "",
-            pathname: "**"
-        }]
-    },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "armada-ais-files.s3.amazonaws.com",
+        port: "",
+        pathname: "**"
+      },
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com",
+        port: "",
+        pathname: "**"
+      }
+    ]
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/recruitment",
+        destination: "/student/recruitment",
+        permanent: true
+      }
+    ]
+  }
 }
 
 export default nextConfig
