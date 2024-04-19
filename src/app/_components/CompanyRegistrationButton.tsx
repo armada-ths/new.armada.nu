4f5faa17cd5f404dc5c801ca7bc917b78b1daeab
Ghali Adyo Putra <gaputra@kth.se>
2024-04-19 18:44:01 +0200

    Created CompanyRegistrationButton

---
+++ src/app/_components/CompanyRegistrationButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export function CompanyRegistrationButton(){
+	return (
+		<Link href="https://register.armada.nu/register">
+			<Button>Exhibitor signup</Button>
+		</Link>
+	)
+}
