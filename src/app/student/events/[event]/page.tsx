ef1a2707fd0542b089f6375820b5d51b011b7d71
vmork <vallemork@gmail.com>
2024-04-29 19:54:35 +0200

    add event card component

--- src/app/student/events/[event]/page.tsx
+++ src/app/student/events/[event]/page.tsx
@@ -1,35 +1,23 @@
+import EventCard from "@/app/student/events/_components/EventCard"
 import { Page } from "@/components/shared/Page"
 import Image from "next/image"
+import { notFound } from "next/navigation"
 
-function formatDate(date: Date) {
-
-}
 
 export default async function EventsPage(props: { params: { event: string } }) {
 	const id = props.params.event
 
 	const response = await fetch("https://ais.armada.nu/api/events")
-	const events = await response.json() as any[]
+	const events = (await response.json()) as any[]
 
 	const event = events.find(event => event.id == id)
-	
-	const startString = new Date(event.event_start * 1000).toLocaleDateString()
-	const endString = new Date(event.event_start * 1000).toLocaleDateString()
+	if (!event) return notFound()
 
 	return (
 		<Page.Background withIndents>
 			<Page.Boundary>
 				<Page.Header>{event.name}</Page.Header>
-				<div>Description: {event.description}</div>
-				<div>Location: {event.location}</div>
-				<div>Start: {startString}</div>
-				<div>End: {startString}</div>
-
-				<Image src={event.image_url} alt="" width={100} height={100}/>
-
-				<div>
-					<pre>{JSON.stringify(event, null, 2)}</pre>
-				</div>
+					<EventCard event={event}></EventCard>
 			</Page.Boundary>
 		</Page.Background>
 	)
