6396875cd27b38db89a687c254ef7a247a399344
Hampus Hallkvist <hampus@hallkvist.org>
2024-05-06 19:40:30 +0200

    feat: Add featureflag support

--- src/feature_flags.ts
+++ src/feature_flags.ts
@@ -1,14 +1,10 @@
 import { FlagDefinitionsType } from "@vercel/flags"
 
-const FEATURE_FLAG_DEFINITIONS = {
-	test: {}
-} satisfies FlagDefinitionsType
+export const FEATURE_FLAG_DEFINITIONS = {} satisfies FlagDefinitionsType
 
 export const FEATURE_FLAGS: Record<
 	keyof typeof FEATURE_FLAG_DEFINITIONS,
 	boolean
-> = {
-	test: true
-}
+> = {}
 
-export default FEATURE_FLAG_DEFINITIONS
+export default FEATURE_FLAGS
