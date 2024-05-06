b6f4f00a0478d24d37150ee8d1fe5fb2249ae381
Hampus Hallkvist <hampus@hallkvist.org>
2024-05-06 19:33:05 +0200

    fix: Add support for feature flags

--- src/app/.well-known/vercel/flags/route.ts
+++ src/app/.well-known/vercel/flags/route.ts
@@ -1,10 +1,17 @@
 import { type ApiData } from "@vercel/flags"
 import { NextResponse } from "next/server"
-import { FEATURE_FLAG_DEFINITIONS } from "../../../../feature_flags"
 
 export async function GET() {
 	const apiData = {
-		definitions: FEATURE_FLAG_DEFINITIONS
+		definitions: {
+			events: {
+				description: "Show event page",
+				options: [
+					{ value: false, label: "Off" },
+					{ value: true, label: "On" }
+				]
+			}
+		}
 	}
 	return NextResponse.json<ApiData>(apiData)
 }
--- src/feature_flags.ts
+++ src/feature_flags.ts
@@ -1,10 +1,14 @@
 import { FlagDefinitionsType } from "@vercel/flags"
 
-export const FEATURE_FLAG_DEFINITIONS = {} satisfies FlagDefinitionsType
+const FEATURE_FLAG_DEFINITIONS = {
+	test: {}
+} satisfies FlagDefinitionsType
 
 export const FEATURE_FLAGS: Record<
 	keyof typeof FEATURE_FLAG_DEFINITIONS,
 	boolean
-> = {}
+> = {
+	test: true
+}
 
-export default FEATURE_FLAGS
+export default FEATURE_FLAG_DEFINITIONS
