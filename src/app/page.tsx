4ca9ff0cde9964c26023ba6ec18a64bd926344d2
Leonard Hökby <bing.hawk@telia.com>
2024-05-20 15:49:30 +0200

    New text for front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,8 +28,7 @@ export default async function HomePage() {
 									Shape your future
 								</h1>
 								<h2 className="my-5 text-stone-300">
-									Where Sweden&apos;s future top engineers come in contact with
-									innovation and career opportunities
+									The No. 1 career fair at KTH Royal Institute of Technology. Where future engineers come in contact with career opportunities and shape their future
 								</h2>
 								<div className="mt-4 flex flex-wrap gap-2">
 									<CompanyRegistrationButton />
