2ec074efa090a655295d82751e6e8fc4d58e0413
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2024-07-05 11:25:34 +0200

    make organisation graphic dynamic, add and improve variables, improve comments

--- src/app/_components/OrganisationMembersGraphic.tsx
+++ src/app/_components/OrganisationMembersGraphic.tsx
@@ -1,48 +1,55 @@
 "use client"
 import { useEffect, useState } from "react"
 
-//create layers, 1 manager, multiple pg, more ot, most hosts
+/*
+POSSIBLE IMPROVEMENTS:
+amount of layers change dynamically
+...
+*/
 
 export function OrganisationMembersGraphic() {
+	const memberRadius = 4
+	const layerRadius = 10
+	const updateSpeed = 50 //animation speed in milliseconds
 	const PgCircleList = (people: number) => {
 		const [theta, setTheta] = useState(0)
 
 		let pi_division = 0
 		let pi_division2 = 0
 		let pg_circles = []
+		let pg_layer1 = Math.floor(Math.PI / Math.asin(memberRadius / layerRadius)) //amount of circles of radius [memberRadius] that can fit on the circle making up a layer
 
-		//radius = 4, diameter = 8, 3 circles min length = 24, if length is exceeded create new layer
-		if (people > 7) {
-			pi_division = (2 * Math.PI) / Math.floor(2 * Math.PI + 1)
-			pi_division2 = (2 * Math.PI) / (people - Math.floor(2 * Math.PI + 1))
+		//divide pi by people, if divided num too small -> add second layer
+		if (people > pg_layer1) {
+			pi_division = (2 * Math.PI) / pg_layer1
+			pi_division2 = (2 * Math.PI) / (people - pg_layer1)
 		} else {
 			pi_division = (2 * Math.PI) / people
 		}
-
-		//divide pi by people, if divided num too small -> add second layer
-		for (let i = 0; i < people; i++) {
+		//setting up layers
+		for (let i = 0; i < Math.min(people, pg_layer1); i++) {
 			pg_circles.push({
 				name: i,
-				cx: 75 + 10 * Math.sin(i * pi_division + theta),
-				cy: 75 + 10 * Math.cos(i * pi_division + theta),
-				r: 4.2
+				cx: 85 + layerRadius * Math.sin(i * pi_division + theta),
+				cy: 85 + layerRadius * Math.cos(i * pi_division + theta),
+				r: memberRadius
 			})
 		}
-		for (let i = 7; i < people; i++) {
+		for (let i = pg_layer1; i < people; i++) {
 			pg_circles.push({
 				name: i,
-				cx: 75 + 20 * Math.sin(i * pi_division2 - theta),
-				cy: 75 + 20 * Math.cos(i * pi_division2 - theta),
-				r: 4.2
+				cx: 85 + 2 * layerRadius * Math.sin(i * pi_division2 - theta),
+				cy: 85 + 2 * layerRadius * Math.cos(i * pi_division2 - theta),
+				r: memberRadius
 			})
 		}
-
+		//animating the circles
 		useEffect(() => {
 			const updateTheta = () => {
 				setTheta(prevTheta => (prevTheta + 0.002) % (2 * Math.PI))
 			}
 
-			const interval = setInterval(updateTheta, 50)
+			const interval = setInterval(updateTheta, updateSpeed)
 
 			return () => clearInterval(interval)
 		}, [])
@@ -55,40 +62,60 @@ export function OrganisationMembersGraphic() {
 
 		let pi_division = 0
 		let pi_division2 = 0
+		let pi_division3 = 0
 		let ot_circles = []
+		//amount of circles of radius [memberRadius] that can fit on the circle making up a layer
+		let ot_layer1 = Math.floor(
+			Math.PI / Math.asin(memberRadius / (3 * layerRadius))
+		)
+		let ot_layer2 = Math.floor(
+			Math.PI / Math.asin(memberRadius / (4 * layerRadius))
+		)
 
-		//radius = 4, diameter = 8, 3 circles min length = 24, if length is exceeded create new layer
-		if (people > 25) {
-			pi_division = (2 * Math.PI) / Math.floor(6 * Math.PI + 1)
-			pi_division2 = (2 * Math.PI) / (people - Math.floor(14 * Math.PI + 1))
+		//divide pi by people, if divided num too small -> add layers
+		if (people > ot_layer1 + ot_layer2) {
+			pi_division = (2 * Math.PI) / ot_layer1
+			pi_division2 = (2 * Math.PI) / ot_layer2
+			pi_division3 = (2 * Math.PI) / (people - ot_layer1 - ot_layer2)
+		} else if (people > ot_layer1) {
+			pi_division = (2 * Math.PI) / ot_layer1
+			pi_division2 = (2 * Math.PI) / (people - ot_layer1)
 		} else {
 			pi_division = (2 * Math.PI) / people
 		}
 
-		//divide pi by people, if divided num too small -> add second layer
-		for (let i = 0; i < people; i++) {
+		//setting up layers
+		for (let i = 0; i < Math.min(people, ot_layer1); i++) {
 			ot_circles.push({
 				name: i,
-				cx: 75 + 30 * Math.sin(i * pi_division + theta),
-				cy: 75 + 30 * Math.cos(i * pi_division + theta),
-				r: 4.2
+				cx: 85 + 3 * layerRadius * Math.sin(i * pi_division + theta),
+				cy: 85 + 3 * layerRadius * Math.cos(i * pi_division + theta),
+				r: memberRadius
 			})
 		}
-		for (let i = 25; i < people; i++) {
+		for (let i = ot_layer1; i < ot_layer1 + ot_layer2; i++) {
 			ot_circles.push({
 				name: i,
-				cx: 75 + 40 * Math.sin(i * pi_division2 - theta),
-				cy: 75 + 40 * Math.cos(i * pi_division2 - theta),
-				r: 4.2
+				cx: 85 + 4 * layerRadius * Math.sin(i * pi_division2 - theta),
+				cy: 85 + 4 * layerRadius * Math.cos(i * pi_division2 - theta),
+				r: memberRadius
 			})
 		}
-
+		for (let i = ot_layer1 + ot_layer2; i < people; i++) {
+			ot_circles.push({
+				name: i,
+				cx: 85 + 5 * layerRadius * Math.sin(i * pi_division3 + theta),
+				cy: 85 + 5 * layerRadius * Math.cos(i * pi_division3 + theta),
+				r: memberRadius
+			})
+		}
+		//animating the circles
 		useEffect(() => {
 			const updateTheta = () => {
-				setTheta(prevTheta => (prevTheta + 0.002) % (2 * Math.PI))
+				setTheta(prevTheta => (prevTheta + 0.0015) % (2 * Math.PI))
 			}
 
-			const interval = setInterval(updateTheta, 50)
+			const interval = setInterval(updateTheta, updateSpeed)
 
 			return () => clearInterval(interval)
 		}, [])
@@ -101,40 +128,60 @@ export function OrganisationMembersGraphic() {
 
 		let pi_division = 0
 		let pi_division2 = 0
+		let pi_division3 = 0
 		let host_circles = []
+		//amount of circles of radius [memberRadius] that can fit on the circle making up a layer
+		let host_layer1 = Math.floor(
+			Math.PI / Math.asin(memberRadius / (6 * layerRadius))
+		)
+		let host_layer2 = Math.floor(
+			Math.PI / Math.asin(memberRadius / (7 * layerRadius))
+		)
 
-		//radius = 4, diameter = 8, 3 circles min length = 24, if length is exceeded create new layer
-		if (people > 50) {
-			pi_division = (2 * Math.PI) / Math.floor(11 * Math.PI + 1)
-			pi_division2 = (2 * Math.PI) / (people - Math.floor(25 * Math.PI + 1))
+		//divide pi by people, if divided num too small -> add layers
+		if (people > host_layer1 + host_layer2) {
+			pi_division = (2 * Math.PI) / host_layer1
+			pi_division2 = (2 * Math.PI) / host_layer2
+			pi_division3 = (2 * Math.PI) / (people - host_layer1 - host_layer2)
+		} else if (people > host_layer1) {
+			pi_division = (2 * Math.PI) / host_layer1
+			pi_division2 = (2 * Math.PI) / (people - host_layer1)
 		} else {
 			pi_division = (2 * Math.PI) / people
 		}
 
-		//divide pi by people, if divided num too small -> add second layer
-		for (let i = 0; i < people; i++) {
+		//setting up layers
+		for (let i = 0; i < Math.min(people, host_layer1); i++) {
 			host_circles.push({
 				name: i,
-				cx: 75 + 50 * Math.sin(i * pi_division + theta),
-				cy: 75 + 50 * Math.cos(i * pi_division + theta),
-				r: 4.2
+				cx: 85 + 6 * layerRadius * Math.sin(i * pi_division - theta),
+				cy: 85 + 6 * layerRadius * Math.cos(i * pi_division - theta),
+				r: memberRadius
 			})
 		}
-		for (let i = 50; i < people; i++) {
+		for (let i = host_layer1; i < host_layer1 + host_layer2; i++) {
 			host_circles.push({
 				name: i,
-				cx: 75 + 60 * Math.sin(i * pi_division2 - theta),
-				cy: 75 + 60 * Math.cos(i * pi_division2 - theta),
-				r: 4.2
+				cx: 85 + 7 * layerRadius * Math.sin(i * pi_division2 + theta),
+				cy: 85 + 7 * layerRadius * Math.cos(i * pi_division2 + theta),
+				r: memberRadius
 			})
 		}
-
+		for (let i = host_layer1 + host_layer2; i < people; i++) {
+			host_circles.push({
+				name: i,
+				cx: 85 + 8 * layerRadius * Math.sin(i * pi_division3 - theta),
+				cy: 85 + 8 * layerRadius * Math.cos(i * pi_division3 - theta),
+				r: memberRadius
+			})
+		}
+		//animating the circles
 		useEffect(() => {
 			const updateTheta = () => {
-				setTheta(prevTheta => (prevTheta + 0.002) % (2 * Math.PI))
+				setTheta(prevTheta => (prevTheta + 0.001) % (2 * Math.PI))
 			}
 
-			const interval = setInterval(updateTheta, 50)
+			const interval = setInterval(updateTheta, updateSpeed)
 
 			return () => clearInterval(interval)
 		}, [])
@@ -154,7 +201,7 @@ export function OrganisationMembersGraphic() {
 		)
 	})
 
-	const otCircles = OtCircleList(70).map(circle => {
+	const otCircles = OtCircleList(80).map(circle => {
 		return (
 			<circle
 				key={circle.name}
@@ -180,8 +227,8 @@ export function OrganisationMembersGraphic() {
 
 	return (
 		<>
-			<svg viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
-				<circle fill="orange" cx="75" cy="75" r="4.2" />
+			<svg viewBox="0 0 170 170" xmlns="http://www.w3.org/2000/svg">
+				<circle fill="orange" cx="85" cy="85" r={memberRadius} />
 				{pgCircles}
 				{otCircles}
 				{hostCircles}
