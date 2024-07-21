76d62ae9d9f0878dd4e266465458fd49aec2be9c
vmork <vallemork@gmail.com>
2024-07-22 00:10:46 +0200

    scale booth markers on zoom color booths differently on hover

--- src/app/student/map/_components/BoothMarkers.tsx
+++ src/app/student/map/_components/BoothMarkers.tsx
@@ -1,37 +1,44 @@
 "use client"
 
-import { Booth, BoothMap } from "@/app/student/map/lib/types"
 import Image from "next/image"
 import { Marker } from "react-map-gl/maplibre"
+import { Booth, BoothMap } from "../lib/booths"
 
-function BoothMarker({ booth }: { booth: Booth }) {
+function BoothMarker({ booth, scale }: { booth: Booth; scale: number }) {
 	const logoSrc = booth.exhibitor.logo_squared ?? booth.exhibitor.logo_freesize
 	return (
 		<Marker
 			key={booth.id}
 			longitude={booth.center[0]}
 			latitude={booth.center[1]}>
-			{logoSrc ? (
-				<div className="">
-					<Image
-						className="h-16 w-16"
-						src={logoSrc}
-						alt={booth.exhibitor.name}
-						width={300}
-						height={300}></Image>
-				</div>
-			) : (
-				<span className="text-black">{booth.exhibitor.name}</span>
-			)}
+			<div className="size-[50px]" style={{ transform: `scale(${scale})` }}>
+				{logoSrc ? (
+					<div>
+						<Image
+							src={logoSrc}
+							alt={booth.exhibitor.name}
+							width={300}
+							height={300}></Image>
+					</div>
+				) : (
+					<span className="text-black">{booth.exhibitor.name}</span>
+				)}
+			</div>
 		</Marker>
 	)
 }
 
-export async function BoothMarkers({ boothMap }: { boothMap: BoothMap }) {
+export function BoothMarkers({
+	boothMap,
+	scale
+}: {
+	boothMap: BoothMap
+	scale: number
+}) {
 	return (
 		<>
 			{Array.from(boothMap.values()).map(booth => (
-				<BoothMarker key={booth.id} booth={booth} />
+				<BoothMarker key={booth.id} booth={booth} scale={scale} />
 			))}
 		</>
 	)
