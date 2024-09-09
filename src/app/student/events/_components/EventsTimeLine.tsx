ae1e3f97d12cd7a92c4143b35b3d0678cd2f9bdc
Duosi-Dai <daiduosi1999@gmail.com>
2024-09-09 18:57:59 +0200

    event page updated

--- src/app/student/events/_components/EventsTimeLine.tsx
+++ src/app/student/events/_components/EventsTimeLine.tsx
@@ -7,25 +7,25 @@ import { Suspense } from "react"
 export function EventsTimeline({ events }: { events: Event[] }) {
   return (
     <>
-      <Alert className="my-5">
-        <AlertTitle>No events available at the moment</AlertTitle>
-        <AlertDescription>
-          Follow us on{" "}
-          <Link
-            className="text-white underline hover:no-underline"
-            href={"https://www.instagram.com/thsarmada/"}>
-            instagram
-          </Link>{" "}
-          for latest news!
-        </AlertDescription>
-      </Alert>
+      {events.length === 0 && (
+        <Alert className="my-5">
+          <AlertTitle>No events available at the moment</AlertTitle>
+          <AlertDescription>
+            Follow us on{" "}
+            <Link
+              className="text-white underline hover:no-underline"
+              href={"https://www.instagram.com/thsarmada/"}>
+              instagram
+            </Link>{" "}
+            for latest news!
+          </AlertDescription>
+        </Alert>
+      )}
       <div className="relative mt-10 border-s border-melon-700">
         {events.map(event => (
           // EventItem uses useSearchParams, so needs to have a Suspense boundary
           <Suspense key={event.id}>
-            {!event.name.includes("[TEST]") && (
-              <EventItem event={event}></EventItem>
-            )}
+            <EventItem event={event}></EventItem>
           </Suspense>
         ))}
       </div>
