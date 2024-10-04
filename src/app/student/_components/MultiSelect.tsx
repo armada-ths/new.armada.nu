bedee1a94cb756537f70d85185475af5a3f200c9
AmiyaSX <2668590277@qq.com>
2024-10-04 08:20:59 +0200

    Fix filter code duplicate

--- src/app/student/_components/MultiSelect.tsx
+++ src/app/student/_components/MultiSelect.tsx
@@ -1,5 +1,5 @@
-import { Filter, FilterItem } from "@/app/student/_components/ExhibitorFilters"
 import BadgeCollection from "@/app/student/exhibitors/_components/BadgeCollection"
+import { Filter, FilterItem } from "@/app/student/lib/filters"
 import { useScreenSize } from "@/components/shared/hooks/useScreenSize"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
--- src/app/student/exhibitors/_components/ExhibitorList.tsx
+++ src/app/student/exhibitors/_components/ExhibitorList.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import ExhibitorFilters from "@/app/student/_components/ExhibitorFilters"
+import ExhibitorListFilteringHeader from "@/app/student/_components/ExhibitorListFilteringHeader"
 import { ExhibitorCard } from "@/app/student/exhibitors/_components/ExhibitorCard"
 import { Exhibitor } from "@/components/shared/hooks/api/useExhibitors"
 import {
@@ -47,7 +47,7 @@ export function ExhibitorList({
   return (
     <div className="mt-10">
       <div className="flex justify-between">
-        <ExhibitorFilters
+        <ExhibitorListFilteringHeader
           key={year} // reset filters when year changes
           exhibitors={exhibitors}
           onChange={setFilteredExhibitors}
