1586323ea0bc0ec3d06e67d7294778e86707541e
Hampus Hallkvist <hampus@hallkvist.org>
2024-11-07 20:18:26 +0100

    fix: Attempt at fixing feature flag

--- src/components/shared/feature.ts
+++ src/components/shared/feature.ts
@@ -1,9 +1,28 @@
 import featureFlags from "@/feature_flags"
 import Cookies from "js-cookie"
+import { cookies } from "next/headers"
 
 export function feature(feature: keyof typeof featureFlags) {
-  const rawOverrides = Cookies.get("vercel-flag-overrides")
+  // Server side
+  if (window === null) {
+    const cookie = cookies()
 
+    const result = cookie.get("vercel-flag-overrides")
+    if (result != null) {
+      return parseCookie(feature, result.value)
+    }
+    return null
+  }
+
+  // Client side
+  const rawOverrides = Cookies.get("vercel-flag-overrides")
+  return parseCookie(feature, rawOverrides)
+}
+
+function parseCookie(
+  feature: keyof typeof featureFlags,
+  rawOverrides: string | undefined
+) {
   const overrides =
     rawOverrides == null
       ? {}
