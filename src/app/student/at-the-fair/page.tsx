53cc73aa3b787927eef05b2b780aa649dde3098d
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2024-11-09 15:20:17 +0100

    small edit to at fair page content

--- src/app/student/at-the-fair/page.tsx
+++ src/app/student/at-the-fair/page.tsx
@@ -1,3 +1,4 @@
+import { P } from "@/app/_components/Paragraph"
 import { Page } from "@/components/shared/Page"
 
 export default async function AtFairPage() {
@@ -6,12 +7,33 @@ export default async function AtFairPage() {
       <Page.Boundary>
         <Page.Header>At the Fair</Page.Header>
         <div>
-          <ul>
-            <li>1.</li>
-            <li>2.</li>
-            <li>3.</li>
-          </ul>
+          <P>
+            Walking up to a representative of a company you really want to work
+            for can be intimidating! But don't fret, below we’ve collected some
+            tips on how to get the most out of your conversation.
+          </P>
+          <P>
+            <ul>
+              <li>
+                <i>· Formulate your sentences and speak clearly.</i>
+              </li>
+              <li>
+                <i>· Treat it like a friendly chat.</i>
+              </li>
+              <li>
+                <i>· Show interest and ask questions.</i>
+              </li>
+            </ul>
+          </P>
+          <P>
+            Remember that you are not chatting with an AI who just knows facts
+            about the company, but with another person. Think of it as a date
+            with a company!
+          </P>
         </div>
+        <br />
+        <Page.Header tier="secondary">Example questions</Page.Header>
+        {/*INSERT QUESTION GENERATOR HERE*/}
       </Page.Boundary>
     </Page.Background>
   )
