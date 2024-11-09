2e69f711e1d004d4e7c959b8ad3388d96baa672f
Erik Sevelin <91209290+SortHvit@users.noreply.github.com>
2024-11-09 16:34:50 +0100

    Add question component to at the fair page

--- src/app/student/at-the-fair/page.tsx
+++ src/app/student/at-the-fair/page.tsx
@@ -12,19 +12,19 @@ export default async function AtFairPage() {
             for can be intimidating! But don't fret, below we’ve collected some
             tips on how to get the most out of your conversation.
           </P>
-          <P>
-            <ul>
-              <li>
-                <i>· Formulate your sentences and speak clearly.</i>
-              </li>
-              <li>
-                <i>· Treat it like a friendly chat.</i>
-              </li>
-              <li>
-                <i>· Show interest and ask questions.</i>
-              </li>
-            </ul>
-          </P>
+
+          <ul className="mt-2 text-stone-400">
+            <li>
+              <i>· Formulate your sentences and speak clearly.</i>
+            </li>
+            <li>
+              <i>· Treat it like a friendly chat.</i>
+            </li>
+            <li>
+              <i>· Show interest and ask questions.</i>
+            </li>
+          </ul>
+
           <P>
             Remember that you are not chatting with an AI who just knows facts
             about the company, but with another person. Think of it as a date
@@ -33,7 +33,24 @@ export default async function AtFairPage() {
         </div>
         <br />
         <Page.Header tier="secondary">Example questions</Page.Header>
+        <P>
+          Below are some examples of questions you can ask the companies during
+          Armada.
+        </P>
         {/*INSERT QUESTION GENERATOR HERE*/}
+        <div className="mt-2 flex min-h-48 min-w-48 rounded-2xl bg-green-950 p-5">
+          <p className="absolute text-green-300 opacity-80">
+            <i>Question</i>
+          </p>
+          <div className="flex-grow place-content-center justify-center">
+            <p className="text-center text-3xl text-green-50 opacity-90">
+              <i>
+                If you could give your younger self advice about working in your
+                field, what would it be?
+              </i>
+            </p>
+          </div>
+        </div>
       </Page.Boundary>
     </Page.Background>
   )
