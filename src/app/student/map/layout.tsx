6a12aa4cb95b8da798f88d87585c458115095e2b
Hampus Hallkvist <hampus@hallkvist.org>
2024-11-17 12:15:06 +0100

    fix: Layout viewport
    Co-authored-by: Didrik Munther <dmu08@gmail.com>

--- src/app/student/map/layout.tsx
+++ src/app/student/map/layout.tsx
@@ -1,4 +1,12 @@
+import { Viewport } from "next"
 import "./map.css"
+
+export const viewport: Viewport = {
+  themeColor: "black",
+  width: "device-width, initial-scale=1, maximum-scale=1",
+  height: "device-height, initial-scale=1, shrink-to-fit=no"
+}
+
 export default function Layout({ children }: React.PropsWithChildren) {
   return children
 }
